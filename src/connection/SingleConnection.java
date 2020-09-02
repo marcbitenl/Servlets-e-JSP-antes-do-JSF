f9aa9960b143aae9a04bd510322cb03586f98457
marcbitenl <marcbitenl@gmail.com>
2020-09-02 15:45:52 -0300

    Filter e conexão em JDBC - Parte 2

--- src/connection/SingleConnection.java
+++ src/connection/SingleConnection.java
@@ -1,5 +1,47 @@
 package connection;
 
-public class SingleConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-}
+/**
+ * Resposável por fazer a conexão com banco de dados
+ * @author Marcos
+ *
+ */
+
+public class SingleConnection {
+	
+	private static String banco = "jdbc:postgresql://localhost:5432/curso-jsp?autoReconnect=true";
+	private static String password = "Mark8129";
+	private static String user = "postgress";
+	private static Connection connection = null;
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+	
+	private static void conectar() {
+		try {
+			
+			if(connection == null) {
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(banco, user, password);
+				connection.setAutoCommit(false);
+				
+			}
+			
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao conectar com banco de dados");
+		}}
+		
+		public static Connection getConnection() {
+			return connection;
+		}
+	}
+	
+
+
